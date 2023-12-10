08c1ab114bbd7b30a81494eaca0b0a6147cf0c3e
Boris Dovčík <bdovcik@gmail.com>
2023-12-10 13:19:33 +0100

    CLION

--- parallel-assignment-2.cpp
+++ parallel-assignment-2.cpp
@@ -1,11 +1,32 @@
 // parallel-assignment-2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
-#include <iostream>
+#include <mpi.h>
+#include <cstdio>
 
-int main()
+int main(int argc, char** argv)
 {
-    std::cout << "Hello World!\n";
+    // Initialize the MPI environment
+    MPI_Init(NULL, NULL);
+
+    // Get the number of processes
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    // Get the rank of the process
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    // Get the name of the processor
+    char processor_name[MPI_MAX_PROCESSOR_NAME];
+    int name_len;
+    MPI_Get_processor_name(processor_name, &name_len);
+
+    // Print off a hello world message
+    printf("Hello world from processor %s, rank %d out of %d processors\n",
+           processor_name, world_rank, world_size);
+
+    // Finalize the MPI environment.
+    MPI_Finalize();
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
